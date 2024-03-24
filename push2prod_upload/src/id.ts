5e7d0132ddf2b7b27e21a32b7d6ea95fc8f7c88e
Ashutosh JHa <ashutoshj.cs.21@nitj.ac.in>
2024-03-24 15:35:55 +0530

    remaking the whole project from a different and scallable approach

--- push2prod_upload/src/id.ts
+++
@@ -1,9 +0,0 @@
-const MAX_LEN = 5; 
-export function generate(){
-    let ans = "";
-    const subset = "0123456789abcdefghijklmnopqrstuvwxyz";
-    for(let i = 0; i < MAX_LEN; i++){
-        ans += subset[Math.floor(Math.random() *subset.length)];
-    }
-    return ans;
-}
